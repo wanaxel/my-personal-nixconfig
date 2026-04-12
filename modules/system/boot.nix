79942257b19a094e450051835ecb98ce4ad908f3
System administrator <wanaxel@proton.me>
2026-04-12 19:32:23 +0800

    fix: my nvme had some issue i added some parameter to boot.nix

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -6,4 +6,5 @@
   boot.loader.timeout = 20;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.kernelModules = ["i2c-dev"];
+  boot.kernelParams = [ "nvme_core.default_ps_max_latency_us=0" ];
 }
